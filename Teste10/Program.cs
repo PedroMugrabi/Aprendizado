31627f4360c00f1b517d0a7c759d9124839dede4
PedroMugrabi <94012878+PedroMugrabi@users.noreply.github.com>
2022-05-26 10:10:39 -0300

    Teste

---
+++ Teste10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Teste10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numberGrid =
+            {
+             {1, 2 },
+             {3, 4 },
+             {5, 6 },
+            
+            };
+            Console.WriteLine(numberGrid[1,1]);
+        }
+    }
+}
